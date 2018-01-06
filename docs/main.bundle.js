d18a0b188e4556d07cb164b8ce7458f5d3ae7b9c
dsalex1 <dsalex1@ymail.com>
2018-01-06 23:41:06 +0100

    Add files via upload

--- docs/main.bundle.js
+++ docs/main.bundle.js
@@ -21,7 +21,7 @@ exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-b
 
 
 // module
-exports.push([module.i, "header{\r\n    position:fixed;\r\n    top:0;\r\n    left:0;\r\n    width:100%;\r\n    height:70px;\r\n    z-index:999;\r\n    background-Color:rgba(255,255,255,.9);\r\n}\r\n\r\n.logo{\r\n    padding:7px;\r\n    float:left\r\n}\r\n\r\n#mainNav{\r\n    list-style-type:none;\r\n    padding:28px 20px 5px 5px;\r\n    text-align:right;\r\n    width:80%;\r\n    width:calc(100% - 170px);\r\n\r\n    float:left;\r\n}\r\n#mainNav>li{\r\n    display:inline-block;\r\n    margin:0 10px;\r\n    position:relative\r\n}\r\nheader a{\r\n    text-decoration:none;\r\n    color:#000;\r\n}\r\n", ""]);
+exports.push([module.i, "", ""]);
 
 // exports
 
@@ -34,7 +34,7 @@ module.exports = module.exports.toString();
 /***/ "../../../../../src/app/app.component.html":
 /***/ (function(module, exports) {
 
-module.exports = "<nav style=\"position: fixed;top: 0;width: 100%\">\n\n</nav>\n<header>   \n    <div class=\"logo\">\n            <img height=\"55\" src=\"assets/logosmall.png\" />\n    </div>\n    \n    <nav>\n      <ul id=\"mainNav\">\n        <li class=\"mainNaviItemInner\"><a href=\"digitales-schwarzes-brett/funktionen/\">Lorem</a></li>\n        <li class=\"mainNaviItemInner\"><a href=\"digitales-schwarzes-brett/modelle/\">Ipsum</a></li>\n        <li class=\"mainNaviItemInner\"><a href=\"digitales-schwarzes-brett/bafa/\">Dolor</a></li>\n        <li class=\"mainNaviItemInner\"><a href=\"digitales-schwarzes-brett/referenzen/\">Sit Amet</a></li>\n        <li class=\"mainNaviItemInner\"><a href=\"digitales-schwarzes-brett/digitales-mitteilungsbuch/\">Consectetur Adipiscing</a></li>\n        <li class=\"mainNaviItemInner\"><a href=\"digitales-schwarzes-brett/dsbinfopoint/\">Elit</a></li>\n        <li class=\"mainNaviItem\"><a href=\"kontakt/\">Tempor</a></li>\n      </ul>\n    </nav>\n</header>\n\n\n<section >\n  <div style=\"width:100%;height:100vh\" id=\"header\">\n    <div id=\"background\" style=\"overflow:hidden;position:absolute;width:100%;height:100vh;z-index:-2\">\n      <img src=\"assets/background1.png\" style=\"width:100%;height:100%\"/>\n    </div>\n    <div style=\"position:absolute;width:100%;height:100vh;background-color: rgba(0,0,0,0.8);z-index:-1\">\n    </div>\n    <div class=\"ontop\" style=\"text-align: center;  position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%) ;\">\n      <div>\n        <img height=\"300\" src=\"assets/logoPure.svg\" />\n        <h1 style=\"color:white;font-size:3em\"><span style=\"font-weight: bold\">Das</span> neue Anzeigesystem <br/> für Vertretungsplan und Co.</h1>\n      </div>\n    </div>\n  </div>\n</section>\n\n<h1>HTML Ipsum Presents</h1>\n\n<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href=\"#\">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>\n\n<h2>Header Level 2</h2>\n\n<ol>\n   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>\n   <li>Aliquam tincidunt mauris eu risus.</li>\n</ol>\n\n<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>\n\n<h3>Header Level 3</h3>\n\n<ul>\n   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>\n   <li>Aliquam tincidunt mauris eu risus.</li>\n</ul>\n\n"
+module.exports = "<header></header>\n<main></main>\n<footer></footer>"
 
 /***/ }),
 
@@ -76,6 +76,9 @@ AppComponent = __decorate([
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__ = __webpack_require__("../../../platform-browser/@angular/platform-browser.es5.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__app_component__ = __webpack_require__("../../../../../src/app/app.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__navbar_navbar_component__ = __webpack_require__("../../../../../src/app/navbar/navbar.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__main_main_component__ = __webpack_require__("../../../../../src/app/main/main.component.ts");
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__footer_footer_component__ = __webpack_require__("../../../../../src/app/footer/footer.component.ts");
 /* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return AppModule; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -83,6 +86,9 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+
+
+
 
 
 
@@ -94,7 +100,10 @@ var AppModule = (function () {
 AppModule = __decorate([
     __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["b" /* NgModule */])({
         declarations: [
-            __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */]
+            __WEBPACK_IMPORTED_MODULE_2__app_component__["a" /* AppComponent */],
+            __WEBPACK_IMPORTED_MODULE_3__navbar_navbar_component__["a" /* NavbarComponent */],
+            __WEBPACK_IMPORTED_MODULE_4__main_main_component__["a" /* MainComponent */],
+            __WEBPACK_IMPORTED_MODULE_5__footer_footer_component__["a" /* FooterComponent */]
         ],
         imports: [
             __WEBPACK_IMPORTED_MODULE_0__angular_platform_browser__["a" /* BrowserModule */]
@@ -105,6 +114,189 @@ AppModule = __decorate([
 ], AppModule);
 
 //# sourceMappingURL=app.module.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/footer/footer.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/footer/footer.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<p>\n  footer works!\n</p>\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/footer/footer.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return FooterComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var FooterComponent = (function () {
+    function FooterComponent() {
+    }
+    FooterComponent.prototype.ngOnInit = function () {
+    };
+    return FooterComponent;
+}());
+FooterComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_0" /* Component */])({
+        selector: 'app-footer',
+        template: __webpack_require__("../../../../../src/app/footer/footer.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/footer/footer.component.css")]
+    }),
+    __metadata("design:paramtypes", [])
+], FooterComponent);
+
+//# sourceMappingURL=footer.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/main/main.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "header{\r\n    position:fixed;\r\n    top:0;\r\n    left:0;\r\n    width:100%;\r\n    height:70px;\r\n    z-index:999;\r\n    background-Color:rgba(255,255,255,.9);\r\n}\r\n\r\n.logo{\r\n    padding:7px;\r\n    float:left\r\n}\r\n\r\n#mainNav{\r\n    list-style-type:none;\r\n    padding:28px 20px 5px 5px;\r\n    text-align:right;\r\n    width:80%;\r\n    width:calc(100% - 170px);\r\n\r\n    float:left;\r\n}\r\n#mainNav>li{\r\n    display:inline-block;\r\n    margin:0 10px;\r\n    position:relative\r\n}\r\nheader a{\r\n    text-decoration:none;\r\n    color:#000;\r\n}\r\n", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/main/main.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<nav style=\"position: fixed;top: 0;width: 100%\">\n\n</nav>\n<header>   \n    <div class=\"logo\">\n            <img height=\"55\" src=\"assets/logosmall.png\" />\n    </div>\n    \n    <nav>\n      <ul id=\"mainNav\">\n        <li class=\"mainNaviItemInner\"><a href=\"digitales-schwarzes-brett/funktionen/\">Lorem</a></li>\n        <li class=\"mainNaviItemInner\"><a href=\"digitales-schwarzes-brett/modelle/\">Ipsum</a></li>\n        <li class=\"mainNaviItemInner\"><a href=\"digitales-schwarzes-brett/bafa/\">Dolor</a></li>\n        <li class=\"mainNaviItemInner\"><a href=\"digitales-schwarzes-brett/referenzen/\">Sit Amet</a></li>\n        <li class=\"mainNaviItemInner\"><a href=\"digitales-schwarzes-brett/digitales-mitteilungsbuch/\">Consectetur Adipiscing</a></li>\n        <li class=\"mainNaviItemInner\"><a href=\"digitales-schwarzes-brett/dsbinfopoint/\">Elit</a></li>\n        <li class=\"mainNaviItem\"><a href=\"kontakt/\">Tempor</a></li>\n      </ul>\n    </nav>\n</header>\n\n\n<section >\n  <div style=\"width:100%;height:100vh\" id=\"header\">\n    <div id=\"background\" style=\"overflow:hidden;position:absolute;width:100%;height:100vh;z-index:-2\">\n      <img src=\"assets/background1.png\" style=\"width:100%;height:100%\"/>\n    </div>\n    <div style=\"position:absolute;width:100%;height:100vh;background-color: rgba(0,0,0,0.8);z-index:-1\">\n    </div>\n    <div class=\"ontop\" style=\"text-align: center;  position: absolute;top: 50%;left: 50%;transform: translate(-50%,-50%) ;\">\n      <div>\n        <img height=\"300\" src=\"assets/logoPure.svg\" />\n        <h1 style=\"color:white;font-size:3em\"><span style=\"font-weight: bold\">Das</span> neue Anzeigesystem <br/> für Vertretungsplan und Co.</h1>\n      </div>\n    </div>\n  </div>\n</section>\n\n<h1>HTML Ipsum Presents</h1>\n\n<p><strong>Pellentesque habitant morbi tristique</strong> senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. <em>Aenean ultricies mi vitae est.</em> Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, <code>commodo vitae</code>, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. <a href=\"#\">Donec non enim</a> in turpis pulvinar facilisis. Ut felis.</p>\n\n<h2>Header Level 2</h2>\n\n<ol>\n   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>\n   <li>Aliquam tincidunt mauris eu risus.</li>\n</ol>\n\n<blockquote><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus magna. Cras in mi at felis aliquet congue. Ut a est eget ligula molestie gravida. Curabitur massa. Donec eleifend, libero at sagittis mollis, tellus est malesuada tellus, at luctus turpis elit sit amet quam. Vivamus pretium ornare est.</p></blockquote>\n\n<h3>Header Level 3</h3>\n\n<ul>\n   <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>\n   <li>Aliquam tincidunt mauris eu risus.</li>\n</ul>\n\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/main/main.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return MainComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var MainComponent = (function () {
+    function MainComponent() {
+    }
+    MainComponent.prototype.ngOnInit = function () {
+    };
+    return MainComponent;
+}());
+MainComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_0" /* Component */])({
+        selector: 'app-main',
+        template: __webpack_require__("../../../../../src/app/main/main.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/main/main.component.css")]
+    }),
+    __metadata("design:paramtypes", [])
+], MainComponent);
+
+//# sourceMappingURL=main.component.js.map
+
+/***/ }),
+
+/***/ "../../../../../src/app/navbar/navbar.component.css":
+/***/ (function(module, exports, __webpack_require__) {
+
+exports = module.exports = __webpack_require__("../../../../css-loader/lib/css-base.js")(false);
+// imports
+
+
+// module
+exports.push([module.i, "", ""]);
+
+// exports
+
+
+/*** EXPORTS FROM exports-loader ***/
+module.exports = module.exports.toString();
+
+/***/ }),
+
+/***/ "../../../../../src/app/navbar/navbar.component.html":
+/***/ (function(module, exports) {
+
+module.exports = "<p>\n  navbar works!\n</p>\n"
+
+/***/ }),
+
+/***/ "../../../../../src/app/navbar/navbar.component.ts":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__("../../../core/@angular/core.es5.js");
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return NavbarComponent; });
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+
+var NavbarComponent = (function () {
+    function NavbarComponent() {
+    }
+    NavbarComponent.prototype.ngOnInit = function () {
+    };
+    return NavbarComponent;
+}());
+NavbarComponent = __decorate([
+    __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["_0" /* Component */])({
+        selector: 'app-navbar',
+        template: __webpack_require__("../../../../../src/app/navbar/navbar.component.html"),
+        styles: [__webpack_require__("../../../../../src/app/navbar/navbar.component.css")]
+    }),
+    __metadata("design:paramtypes", [])
+], NavbarComponent);
+
+//# sourceMappingURL=navbar.component.js.map
 
 /***/ }),
 
